3b8f519392b7240c2bba7e585c8401c5aec88d9d
Surjya <paiqz8@gmail.com>
2025-09-16 22:14:21 +0530

    changed user id

--- src/test/java/YSK/ErrorValidation.java
+++ src/test/java/YSK/ErrorValidation.java
@@ -14,8 +14,8 @@ public class ErrorValidation extends BaseTest{
 	public void LoginValidation () throws IOException, InterruptedException {
 		
 		// TODO Auto-generated method stub
-		landingPage.loginApplication("axyzds@gmail.com", "Samsung525$");
-		Assert.assertEquals("Incorrect email or password.", landingPage.geterrorMessage());
+		landingPage.loginApplication("axyzds@gmail.com", "Saaaamsung525$");
+		Assert.assertEquals("Incorrect email or password.", landingPage.geterrorMessage());  //need to revisit
 		}
 	
 	@Test(retryAnalyzer=Retry.class)
